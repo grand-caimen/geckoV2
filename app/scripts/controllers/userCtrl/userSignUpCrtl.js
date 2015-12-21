6369fbe9aca95902b847a4b192a25b8120a12504
Festus Kiprop <fyangani@gmail.com>
2015-12-20 22:38:39 -0600

    Signup page

--- app/scripts/controllers/userCtrl/userSignUpCrtl.js
+++ app/scripts/controllers/userCtrl/userSignUpCrtl.js
@@ -1,1 +1,16 @@
-userSigUpCrtl.js
+'use strict';
+
+angular.module( 'myApp' )
+
+.controller( 'UserLoginCtrl',
+    function ( $scope, UserLogin ) {
+      var user = {};
+
+      $scope.userLogin = function() {
+        user.username = $scope.username;
+        user.password = $scope.password;
+        console.log('user: ', user);
+        console.log(UserLogin);
+        UserLogin.userPostLogin(user)
+      }
+    });
